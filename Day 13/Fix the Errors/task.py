3335e9a35b7ee7f91a36942c21f2c488a26b9d0d
Muhammad Awais <awaisch31709@gmail.com>
2026-09-01 18:12:47 +0500

    Day 13

---
+++ Day 13/Fix the Errors/task.py
@@ -0,0 +1,3 @@
+age = (input("How old are you?"))
+if age > 18:
+    print("You can drive at age {age}.")
---
+++ Day 13/Use Print/task.py
@@ -0,0 +1,8 @@
+word_per_page = 0
+pages = int(input("Number of pages: "))
+word_per_page = int(input("Number of words per page: "))
+total_words = pages * word_per_page
+
+print(f"Total Pages: {pages}")
+print(f"Words per Page: {word_per_page}")
+print(f"Total Words: {total_words}")
