fc0eb2ede66c7d332cd2383c9cbf072523987451
Muhammad Awais <awaisch31709@gmail.com>
2026-09-04 14:43:48 +0500

    Day 16

---
+++ Day 16/main.py
@@ -0,0 +1,33 @@
+# from prettytable import PrettyTable
+# table = PrettyTable()
+# table.add_column("Pokemon Name",["Pikachu","Squirtle","Charmander"])
+# table.add_column("Type",["Electric","Water","Fire"])
+# table.align = "l"
+#
+# print(table)
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+is_machine_on = True
+
+while is_machine_on:
+    choice = input(f"What would you like? ({menu.get_items()}): ")
+
+    if choice == "off":
+        is_machine_on = False
+
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+
+    else:
+        drink = menu.find_drink(choice)
+
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
